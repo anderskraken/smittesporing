0da1bdd4475c76e727e58243eab10a88049fd85e
Christopher Dambakk <christopher.dambakk@gmail.com>
2020-03-16 19:09:12 +0100

    Symptoms step 2 layout

--- app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep1.kt
+++ app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep1.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_symptoms.view.*
+import kotlinx.android.synthetic.main.fragment_symptoms_step1.view.*
+import kotlinx.android.synthetic.main.fragment_symptoms_step2.view.*
 import no.agens.covid19.R
 
 class SymptomsStep1 : androidx.fragment.app.Fragment() {
@@ -15,9 +19,10 @@ class SymptomsStep1 : androidx.fragment.app.Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_symptoms_step1, container, false)
 
-
+        root.step1nextButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_symptomsStep1_to_symptomsStep2, null)
+        )
 
         return root
     }
-
 }
