48628a54024e315a7d7a1cc96472de7b5c62ef50
Christopher Dambakk <christopher.dambakk@gmail.com>
2020-03-17 22:03:58 +0100

    Passing info from form to next fragment wip

--- app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep1.kt
+++ app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep1.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_symptoms.view.*
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_symptoms_step1.view.*
-import kotlinx.android.synthetic.main.fragment_symptoms_step2.view.*
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
 import no.agens.covid19.R
+import timber.log.Timber
 
+@UnstableDefault
 class SymptomsStep1 : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(
@@ -19,10 +23,59 @@ class SymptomsStep1 : androidx.fragment.app.Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_symptoms_step1, container, false)
 
-        root.step1nextButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_symptomsStep1_to_symptomsStep2, null)
-        )
+        root.step1nextButton.setOnClickListener {
+            val input = getUserInput(root)
+            if (input != null) {
+                val json = Json.toJson(Symptoms.serializer(), input).toString()
+                Timber.d("User entered this information: $json")
+                val action = SymptomsStep1Directions.actionSymptomsStep1ToSymptomsStep2(json)
+                findNavController().navigate(action)
+            }
+        }
 
         return root
+    }
+
+    private fun getUserInput(root: View): Symptoms? {
+        val genderGroup = root.gender_group
+        val selectedGender = genderGroup.findViewById<RadioButton>(genderGroup.checkedRadioButtonId)
+        val gender = if (selectedGender == null) {
+            "null"
+        } else {
+            selectedGender.text
+        }
+
+        val ageInput = root.ageInput.editText?.text.toString()
+        val age = try {
+            root.ageInput.error = null
+            ageInput.toInt()
+        } catch (e: NumberFormatException) {
+            root.ageInput.error = "Alder må være et gyldig tall"
+            return null
+        }
+
+        val inRiskGroup = root.riskGroup.getYesOrNoResponse()
+
+        val isTested = root.testedGroup.getYesOrNoResponse()
+
+        return Symptoms(
+            gender = gender.toString(),
+            age = age,
+            inRiskGroup = inRiskGroup,
+            testedPositive = isTested
+        )
+    }
+}
+
+
+fun RadioGroup.getYesOrNoResponse(): Boolean {
+    val selected = this.findViewById<RadioButton>(this.checkedRadioButtonId)
+    return if (selected == null) {
+        false
+    } else {
+        when (selected.text) {
+            "Ja" -> true
+            else -> false
+        }
     }
 }
--- app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep2.kt
+++ app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep2.kt
@@ -1,73 +1,35 @@
 package no.agens.covid19.ui.information.Symptoms
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_symptoms_step2.view.*
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
 import no.agens.covid19.R
+import timber.log.Timber
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@UnstableDefault
+class SymptomsStep2 : androidx.fragment.app.Fragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SymptomsStep2.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SymptomsStep2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val args: SymptomsStep2Args by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_symptoms_step2, container, false)
 
-        root.saveButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_symptomsStep2_to_symptomsStep3, null)
-        )
+        val receivedSymptomsFromPrevFrag = args.intermediateSymptoms
+        val intermediateSymptoms = Json.parse(Symptoms.serializer(), receivedSymptomsFromPrevFrag)
+        Timber.d("Got this data from prev fragment: $intermediateSymptoms")
 
-        root.step2BackButton.setOnClickListener {
-            findNavController().navigateUp()
-        }
+        root.saveButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_symptomsStep2_to_symptomsStep3, null))
 
         return root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SymptomsStep2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SymptomsStep2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
