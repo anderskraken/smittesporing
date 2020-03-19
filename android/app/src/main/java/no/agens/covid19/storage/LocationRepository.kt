6c7e8023f7a638ff2bd1f0f1945dd35bdf111e67
Jaran Flaath <jaran.flaath@gmail.com>
2020-03-19 09:42:50 +0100

    Storing location to database

---
+++ android/app/src/main/java/no/agens/covid19/storage/LocationRepository.kt
@@ -0,0 +1,33 @@
+package no.agens.covid19.storage
+
+import android.content.Context
+import androidx.room.*
+import no.agens.covid19.storage.domain.Location
+import timber.log.Timber
+
+class LocationRepository(context: Context) {
+
+    private val database: LocationDatabase
+
+    init {
+        database =
+            Room.databaseBuilder(context, LocationDatabase::class.java, "location_db").build()
+    }
+
+    @Database(entities = [Location::class], version = 1)
+    abstract class LocationDatabase : RoomDatabase() {
+        abstract fun locationDao(): LocationDao
+    }
+
+    @Dao
+    interface LocationDao {
+
+        @Insert
+        fun insert(location: Location)
+    }
+
+    suspend fun addLocations(location: Location) {
+        database.locationDao().insert(location)
+        Timber.d("New location added to database: $location")
+    }
+}
