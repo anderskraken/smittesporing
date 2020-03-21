d7ceb0817358962753e3bdae3120d3584c1b4108
Christopher Dambakk <christopher.dambakk@gmail.com>
2020-03-21 16:01:45 +0100

    Add date pickers and button backgrounds

--- android/app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep2.kt
+++ android/app/src/main/java/no/agens/covid19/ui/information/Symptoms/SymptomsStep2.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_symptoms_step2.view.*
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import no.agens.covid19.R
 import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.*
 
 @UnstableDefault
 class SymptomsStep2 : androidx.fragment.app.Fragment() {
@@ -28,8 +35,49 @@ class SymptomsStep2 : androidx.fragment.app.Fragment() {
         val intermediateSymptoms = Json.parse(Symptoms.serializer(), receivedSymptomsFromPrevFrag)
         Timber.d("Got this data from prev fragment: $intermediateSymptoms")
 
+        root.lastContactInput.setupDatePicker(parentFragmentManager) { selectedDate ->
+
+        }
+
+        root.backToNorwayInput.setupDatePicker(parentFragmentManager) { selectedDate: Date ->
+
+        }
+
         root.saveButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_symptomsStep2_to_symptomsStep3, null))
 
         return root
     }
+}
+
+private fun TextInputLayout.setupDatePicker(fragmentManager: FragmentManager, onDateSelectedCallback: (Date) -> Unit) {
+    editText?.isFocusableInTouchMode = false
+    editText?.setOnClickListener {
+        showDatePicker(this, fragmentManager, onDateSelectedCallback)
+    }
+    setEndIconOnClickListener {
+        showDatePicker(this, fragmentManager, onDateSelectedCallback)
+    }
+}
+
+fun showDatePicker(textInputLayout: TextInputLayout, fragmentManager: FragmentManager, onDateSelectedCallback: (Date) -> Unit) {
+    val now = Calendar.getInstance().timeInMillis
+    val oneYearAgo = Calendar.getInstance()
+    oneYearAgo.set(Calendar.YEAR, 2019)
+    val calendarConstraints = CalendarConstraints.Builder()
+        .setStart(oneYearAgo.timeInMillis)
+        .setEnd(now)
+        .build()
+    val picker = MaterialDatePicker.Builder.datePicker()
+        .setTitleText(R.string.select_date)
+//        .setInputMode(MaterialDatePicker.INPUT_MODE_CALENDAR)
+        .setCalendarConstraints(calendarConstraints)
+        .build()
+
+    picker.addOnPositiveButtonClickListener {
+        val date = Date(it)
+        textInputLayout.helperText = ""
+        textInputLayout.editText?.setText(SimpleDateFormat("d. MMMM").format(date))
+        onDateSelectedCallback(date)
+    }
+    picker.showNow(fragmentManager, "DatePicker")
 }
